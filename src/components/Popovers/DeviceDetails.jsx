ed4d803ba180a06edb11365f6159921d5fdf739a
Shishir A S <shishir.srik@gmail.com>
2020-09-23 03:22:28 +0530

    Device Details popover + mUI

--- src/components/Popovers/DeviceDetails.jsx
+++ src/components/Popovers/DeviceDetails.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-import { Container, Overlay, Tooltip } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Overlay, Popover, Row } from "react-bootstrap";
+import Switch from "@material-ui/core/Switch";
+import Button from "@material-ui/core/Button";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import DesktopWindowsIcon from '@material-ui/icons/DesktopWindows';
 
 const DeviceDetails = ({ target }) => {
+  const [antivirusActive, setAntivirusActive] = useState(false);
+
+  const handleChange = (event) => {
+    setAntivirusActive(event.target.checked);
+  };
+
   return (
     <Overlay
       //target={target.current}
@@ -11,9 +21,35 @@ const DeviceDetails = ({ target }) => {
       placement="bottom"
     >
       {(props) => (
-        <Tooltip id="overlay-example" {...props}>
-          Device Details
-        </Tooltip>
+        <Popover id="popover-basic" {...props}>
+          <Container style={{ width: "200px" }}>
+            <Row style={{ margin: "10px" }} className="justify-content-center">
+              <Button
+                endIcon={<DesktopWindowsIcon />}
+                variant="outlined"
+                color="primary"
+              >
+                Access
+              </Button>
+            </Row>
+            <Row className="justify-content-center">
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={antivirusActive}
+                    color="primary"
+                    onChange={handleChange}
+                    name="antivirusActive"
+                    value={antivirusActive}
+                    inputProps={{ "aria-label": "secondary checkbox" }}
+                  />
+                }
+                label="Antivirus"
+                labelPlacement="start"
+              />
+            </Row>
+          </Container>
+        </Popover>
       )}
     </Overlay>
   );
